04cf199cdefbb80f6f90fe9818e1371cbc4875ee
hdl740 <137336559@qq.com>
2017-10-27 17:23:29 +0800

    asdad

---
+++ WebRoot/WEB-INF/xin/2.0/views/account/myjs/account_index.js
@@ -0,0 +1,11 @@
+$(function(){
+	
+	//点击信存宝-账户余额存入，弹出对话框
+	$(".blue").click(function(){
+		$(".cuenrumain").show();
+	})
+	//点击叉关闭 信存宝
+	$("#closecuenrumain").click(function(){
+		$(".cuenrumain").hide();
+	})
+})
